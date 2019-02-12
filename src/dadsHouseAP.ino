8875ee9efc52f896d41c88b443aefe6f2b06d8c5
petek157 <petek157@Peter-Korugas-MacBook-Pro-1625.local>
2019-02-12 14:33:02 -0600

    Core temp operation set

--- src/dadsHouseAP.ino
+++ src/dadsHouseAP.ino
@@ -5,14 +5,50 @@
  * Date:
  */
 
-// setup() runs once, when the device is first turned on.
+#include "Adafruit_MCP9808.h"
+
+
+Adafruit_MCP9808 tempsensor = Adafruit_MCP9808();
+
+int currentTemp;
+int offset;
+
+unsigned long lastMillis = 0;
+unsigned long delayMillis = 300000;
+
 void setup() {
-  // Put initialization like pinMode and begin functions here.
+
+  Particle.variable("temp", currentTemp);
+  Particle.variable("offset", offset);
+
+  Particle.function("chgOffset", changeOffset);
+  
+  if (!tempsensor.begin()) {
+    while (1);
+  }
 
 }
 
-// loop() runs over and over again, as quickly as it can execute.
 void loop() {
-  // The core of your code will likely live here.
 
+unsigned long currentMillis = millis();
+
+  if (lastMillis == 0 || (currentMillis - lastMillis) > delayMillis) {
+    lastMillis = currentMillis;
+
+    float temp = (((tempsensor.readTempC() * 1.8) + 32) + offset);
+      if (temp > 0 && temp < 100) {
+          if (temp > (currentTemp + 3) || temp < (currentTemp - 3)) {
+            Particle.publish("Temperature", sensorData);
+            currentTemp = temp;
+          }
+      }
+  }
+
+}
+
+int changeOffset(String n_offset) {
+  offset = n_offset.toInt();
+
+  return 1;
 }
